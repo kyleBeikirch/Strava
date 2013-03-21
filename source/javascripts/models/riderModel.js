695ccc629d88ffc638ab91c1095e5e75d570c068
Kyle Beikirch <kyle.beikich@gmail.com>
2013-03-21 11:58:31 -0700

    Start of rider backbone model

---
+++ source/javascripts/models/riderModel.js
@@ -0,0 +1,15 @@
+window.Rider = Backbone.Model.extend({
+    urlRoot:"../api/rider",
+    defaults:{
+        "id":null,
+        "stravaID": ""
+        "name":"",
+        "location":"",
+        "picture":""
+    }
+});
+
+window.Riders = Backbone.Collection.extend({
+    model:Rider,
+    url:"../api/rider"
+});
