21471dfdd25b50c9556b67d953e8e0a1ba538abc
Kyle Beikirch <kyle.beikirch@gmail.com>
2013-03-20 21:56:41 -0700

    Update backbone startView to bind form event

--- source/javascripts/views/startView.js
+++ source/javascripts/views/startView.js
@@ -1,7 +1,5 @@
 window.StartView = Backbone.View.extend({
 
-    tagName:"div", // Not required since 'div' is the default if no el or tagName specified
-
     initialize:function () {
 
         this.template = _.template(tpl.get('start'));
@@ -13,51 +11,14 @@ window.StartView = Backbone.View.extend({
     },
 
     events:{
-        "change input":"change",
-        "click .save":"saveWine",
-        "click .delete":"deleteWine"
+        "submit form#idSubmit": "change",
     },
 
     change:function (event) {
-        var target = event.target;
-        console.log('changing ' + target.id + ' from: ' + target.defaultValue + ' to: ' + target.value);
-        // You could change your model on the spot, like this:
-        // var change = {};
-        // change[target.name] = target.value;
-        // this.model.set(change);
-    },
-
-    saveWine:function () {
-        this.model.set({
-            name:$('#name').val(),
-            grapes:$('#grapes').val(),
-            country:$('#country').val(),
-            region:$('#region').val(),
-            year:$('#year').val(),
-            description:$('#description').val()
-        });
-        if (this.model.isNew()) {
-            var self = this;
-            app.wineList.create(this.model, {
-                success:function () {
-                    app.navigate('wines/' + self.model.id, false);
-                }
-            });
-        } else {
-            this.model.save();
-        }
-
-        return false;
-    },
-
-    deleteWine:function () {
-        this.model.destroy({
-            success:function () {
-                alert('Wine deleted successfully');
-                window.history.back();
-            }
-        });
-        return false;
+        event.preventDefault();
+        var stravaID = this.$('input[name=stravaID]').val();
+ 
+        Backbone.history.navigate('rider/:' + stravaID, true);
     }
 
 });
